929003666c147d75b46d7ad4bd74d6ca27f001a3
toma-s <toma.savkova@gmail.com>
2021-06-19 18:58:04 +0200

    Update mocked be data

--- erni_eats_fe/lib/service/mocked-be-data.dart
+++ erni_eats_fe/lib/service/mocked-be-data.dart
@@ -1,9 +1,10 @@
+// todo implement BE data mocking
 Future getRestaurants() async {
   List<Restaurant> restaurants = [
     Restaurant('Derby pub', RestaurantEstimation(3, 12), 2, 'Pub', 'derby.png', false),
-    Restaurant('Clock block', RestaurantEstimation(5, 54), 3, 'Pub', 'clockblock.png', true),
-    Restaurant('Veg life', RestaurantEstimation(4, 8), 2, 'Pub', 'veglife.png', true),
-    Restaurant('Alfa', RestaurantEstimation(1, 325), 1, 'Pub', 'alfa.png', true),
+    Restaurant('Clock block', RestaurantEstimation(5, 54), 3, 'Restaurant & Beer', 'clockblock.png', true),
+    Restaurant('Veg life', RestaurantEstimation(4, 8), 2, 'Veggie', 'veglife.png', true),
+    Restaurant('Alfa', RestaurantEstimation(1, 325), 1, 'Pub with food', 'alfa.png', true),
     Restaurant('Classic restaurant & Pub', RestaurantEstimation(3, 4), 2, 'Pub', 'alfa.png', false),
   ];
   return await Future.value(restaurants).timeout(const Duration(seconds: 1));
